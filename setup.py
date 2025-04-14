3f752e3c5a01a3471a31b0f7ef9283cca62932a1
Kra Thi <krathi@Kras-Mac-mini.local>
2025-04-13 18:31:15 -0700

    Add GitHub Actions CI and update README/setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_dir / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="matrixx",
-    version="0.1.1",
+    version="0.1.2",
     author="Kranthi",
     author_email="kdevprofile@gmail.com",
     description="Lightweight matrix utility library",
